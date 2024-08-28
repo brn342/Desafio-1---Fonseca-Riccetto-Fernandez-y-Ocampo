f86eea34120114821e2ba6023595c8630fa601e5
brn342 <bruno.ocampo@correo.ucu.edu.uy>
2024-08-28 10:31:58 -0300

    Agrego Board, PrintBOard y modifico Program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,9 +4,14 @@ namespace Ucu.Poo.GameOfLife
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            FileRider fileRider = new FileRider();
+            bool[,] initialBoard = fileRider.LeerArchivo("board.txt");
+            Board board = new Board(initialBoard);
+            Logic nuevaGeneracion = new Logic();
+            PrintBoard printBoard = new PrintBoard(board, nuevaGeneracion);
+
+            printBoard.ImprimirContinuamente();
         }
-    }
 }
